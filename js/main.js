5b4c2b4e637511040d45d9200f03088e4ebb820f
Luka-DV <luka.guva@gmail.com>
2023-09-25 14:25:35 +0200

    add new features v2

--- js/main.js
+++ js/main.js
@@ -1,88 +1,199 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
 
-document.querySelector("button").addEventListener("click", getCocktail);
+document.querySelector("#getCocktail").addEventListener("click", getCocktail);
+document.querySelector("#lucky").addEventListener("click", getRandomCocktail);
+document.querySelector("#ingredient").addEventListener("click", listByIngredient);
 
-/* 
-function getCocktail() {
-    let cocktail = document.querySelector("input").value;
-
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data)
-      data = data.drinks[0] //get the first object in the array
-      document.querySelector("h2").innerText = data.strDrink;
-      document.querySelector("img").src = data.strDrinkThumb;
-      document.querySelector("#instr").innerText = data.strInstructions;
-      for(let i = 1; data[`strIngredient${i}`] != null; i++) {
-        document.querySelector("#ingr").innerText += "\n " + data[`strIngredient${i}`];
-      }
-      
-      
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
-
-} */
-
-
-// with carusel:
 
 let drinkNumber = -1;
 
 let currentCocktail = [];
 
-function checkIfDifferentCocktail(drink) {
-    if(currentCocktail[0] !== currentCocktail[1]){
-        drinkNumber = -1
-        currentCocktail = [drink];
-    }
-}
 
-function getCocktail() {
-    let cocktail = document.querySelector("input").value;
+function getCocktail(fizz) {
+    
+    let cocktail = (typeof fizz === "object") ? document.querySelector("input").value : fizz;
     currentCocktail.unshift(cocktail);
     checkIfDifferentCocktail(cocktail);
 
 
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
-    .then(res => res.json()) // parse response to JS
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
+        .then(res => res.json()) // parse response to JS
 
-    .then(data => {
-      console.log(data)
+        .then(data => {
+            console.log(data);
 
-     /*  if(data.drinks.length < drinkNumber) {
-        drinkNumber = -1
-      } */
-    
-        if(data.drinks.length - 1 === drinkNumber) { //restart counter if at last item of array
-            drinkNumber = -1;
-        }
-    
-      data = data.drinks[++drinkNumber] //get the next object in the array
+            const cocktailsContainer = document.querySelector("#cocktails");
 
-      document.querySelector("h3").innerText = "Ingredients:";
-      document.querySelector("h2").innerText = data.strDrink; //get Name
-      document.querySelector("img").src = data.strDrinkThumb; //get img
-      document.querySelector("#instr").innerText = data.strInstructions; // get instructions
+            if(drinkNumber === data.drinks.length - 1) { //restart counter if at last item of array
+                    drinkNumber = -1;
+            }
 
-      const ulParentElement = document.querySelector("#ingr");
+            while(cocktailsContainer.firstChild) {  //reset cocktail buttons
+                cocktailsContainer.removeChild(cocktailsContainer.firstChild);
+            }
+            document.querySelector("p").innerText = "";
 
-      while(ulParentElement.firstChild) { //remove all of the previous li elements
-        ulParentElement.removeChild(ulParentElement.firstChild)
-      }
+            if(data.drinks.length > 1) {  //create new buttons
+                document.querySelector("p").innerText = "Or did you mean..."
+                for(let i = 1; i <= data.drinks.length - 1; i++) {
+                    const newButtton = document.createElement("button");
+                    newButtton.innerText = data.drinks[i].strDrink;
+                    cocktailsContainer.appendChild(newButtton);
+                }
 
-      for(let i = 1; data[`strIngredient${i}`] != null; i++) { //append neccessary li elements:
-        const listItem = document.createElement("li");
-        listItem.innerText = data[`strIngredient${i}`];
-        ulParentElement.appendChild(listItem);
-      }
-    })
+                document.querySelector("#cocktails").style.justifyContent = "space-between";
 
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+                createEventListeners();
+            }
+
+            
+            drinkData = data.drinks[++drinkNumber]; //get the next object in the array
+
+            document.querySelector("h3").innerText = "Ingredients:";
+            document.querySelector("h2").innerText = drinkData.strDrink; //get Name
+            document.querySelector("img").src = drinkData.strDrinkThumb; //get img
+            document.querySelector("#instr").innerText = drinkData.strInstructions; // get instructions
+
+            const ulParentElement = document.querySelector("#ingr");
+
+            while(ulParentElement.firstChild) { //remove all of the previous li elements
+                ulParentElement.removeChild(ulParentElement.firstChild);
+            }
+
+            for(let i = 1; drinkData[`strIngredient${i}`] != null; i++) { //append neccessary new li elements:
+                const listItem = document.createElement("li");
+                listItem.innerText = drinkData[`strIngredient${i}`];
+                ulParentElement.appendChild(listItem);
+            }
+            })
+
+        .catch(err => {
+            console.log(`error ${err}`);
+        });
 
 }
+
+    function createEventListeners() {
+        const category = document.querySelectorAll("#cocktails > *"); 
+        category.forEach(ele => ele.addEventListener("click", (click) => {
+            getCocktail(click.target.innerText);
+        }));
+    }
+
+    function checkIfDifferentCocktail(drink) {
+        if(currentCocktail[0] !== currentCocktail[1]){
+            drinkNumber = -1;
+            currentCocktail = [drink];
+        } else if (currentCocktail.length === 3) {
+            currentCocktail.pop();
+        }
+    }
+    
+
+    function getRandomCocktail() {
+    
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+            .then(res => res.json()) // parse response to JS
+    
+            .then(data => {
+                console.log(data);
+
+                const cocktailsContainer = document.querySelector("#cocktails");
+
+                while(cocktailsContainer.firstChild) {  //reset cocktail buttons
+                    cocktailsContainer.removeChild(cocktailsContainer.firstChild);
+                }
+                document.querySelector("p").innerText = "";
+
+                drinkData = data.drinks[0]
+    
+                document.querySelector("h3").innerText = "Ingredients:";
+                document.querySelector("h2").innerText = drinkData.strDrink; //get Name
+                document.querySelector("img").src = drinkData.strDrinkThumb; //get img
+                document.querySelector("#instr").innerText = drinkData.strInstructions; // get instructions
+    
+                const ulParentElement = document.querySelector("#ingr");
+    
+                while(ulParentElement.firstChild) { //remove all of the previous li elements
+                    ulParentElement.removeChild(ulParentElement.firstChild);
+                }
+    
+                for(let i = 1; drinkData[`strIngredient${i}`] != null; i++) { //append neccessary new li elements:
+                    const listItem = document.createElement("li");
+                    listItem.innerText = drinkData[`strIngredient${i}`];
+                    ulParentElement.appendChild(listItem);
+                }
+                })
+    
+            .catch(err => {
+                console.log(`error ${err}`);
+            });
+    
+    }
+
+
+    function listByIngredient() {
+
+        const ingredient = document.querySelector("input").value;
+
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+        .then(res => res.json()) // parse response to JS
+
+        .then(data => {
+            console.log(data);
+
+            const cocktailsContainer = document.querySelector("#cocktails");
+            while(cocktailsContainer.firstChild) {  //reset cocktail buttons
+                cocktailsContainer.removeChild(cocktailsContainer.firstChild);
+            }
+            document.querySelector("p").innerText = "";
+
+
+            const ulParentElement = document.querySelector("#ingr");
+            while(ulParentElement.firstChild) { //remove all of the previous li elements
+                ulParentElement.removeChild(ulParentElement.firstChild);
+            }
+
+            document.querySelector("h2").innerText = "";
+            document.querySelector("h3").innerText = "";
+            document.querySelector("h4").innerText = "";
+            document.querySelector("img").src = "";
+
+            document.querySelector("#cocktails").style.justifyContent = "flex-start";
+
+            if(data.drinks.length > 1) {  //create new buttons
+                document.querySelector("p").innerText = "Choose you cocktail:"
+                for(let i = 1; i <= data.drinks.length - 1; i++) {
+                    const newButtton = document.createElement("button");
+                    newButtton.innerText = data.drinks[i].strDrink;
+                    cocktailsContainer.appendChild(newButtton);
+                }
+
+                createEventListeners();
+
+
+                let num = 0;
+                document.querySelectorAll("#cocktails > button").forEach(button => {
+                    if(num % 2 === 0) {
+                        button.style.background = "lightyellow";
+                    } else {
+                        button.style.background = "lightblue";
+                    }
+
+                    num++;
+                })
+
+                document.querySelector("#cocktails").style.fontSize = "clamp(0.8rem, 1.5vw, 1.2rem"
+            }
+
+        })
+
+        .catch(err => {
+            console.log(`error ${err}`);
+        });
+        
+    }
+
+
